9675e629ccbd871bfd4c2f62c5cffe73868a5b84
rmenai <rami.menai@outlook.com>
2022-08-06 01:13:51 +0300

    Make the navigation bar responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { MenuIcon } from "@heroicons/vue/outline";
+
 const config = useRuntimeConfig();
 const github = config.public.github;
 
@@ -13,10 +15,14 @@ const navigation = [
     <div class="mx-auto flex h-16 max-w-8xl items-center justify-between px-4 sm:px-6 lg:px-8 xl:px-10">
       <div class="flex items-center gap-4">
         <img alt="Logo" src="/images/logo.png" height="48" width="29" />
-        <div v-for="nav in navigation" :key="nav">
+        <div v-for="nav in navigation" :key="nav" class="hidden sm:block">
           <NuxtLink :to="nav.href">
             <div class="hover:text-primary">{{ nav.name }}</div>
           </NuxtLink>
+        </div>
+        <!--To do: Add navigation menu on button click-->
+        <div class="block w-6 sm:hidden">
+          <MenuIcon />
         </div>
       </div>
       <a :href="`https://github.com/${github.owner}/${github.repo}`">
