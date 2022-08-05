2aa1dbe392200ad18e4fd4e9b67dc1557467518c
rmenai <rami.menai@outlook.com>
2022-08-06 01:46:03 +0300

    Fix production errors (404 and transpile)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: "src/",
-  build: { transpile: ["konsta"] },
+  build: { transpile: ["konsta", "@heroicons/vue"] },
   buildModules: ["@pinia/nuxt", "@vueuse/nuxt"],
   modules: [
     "@kevinmarrec/nuxt-pwa",
