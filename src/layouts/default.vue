df98178f22e12e0e25a8fe09aa1226de9b025129
rmenai <rami.menai@outlook.com>
2022-08-11 19:47:40 +0300

    Regenerate efiriyad logo

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="min-h-screen bg-gray-100">
+  <Head>
+    <Title>Default</Title>
+    <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <Link rel="manifest" href="/site.webmanifest" />
+    <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2563eb" />
+    <Meta name="msapplication-TileColor" content="#2563eb" />
+    <Meta name="theme-color" content="#ffffff" />
+  </Head>
+
+  <div class="min-h-screen bg-zinc-50">
     <div class="font-inter font-medium text-black">
       <NavBar />
       <main>
