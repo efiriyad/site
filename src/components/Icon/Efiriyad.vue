4673b0156479cf7c8bb901d9adb8b137047ffd4f
rmenai <rami.menai@outlook.com>
2022-08-17 21:42:02 +0300

    Add necessary logos and icons

---
+++ src/components/Icon/Efiriyad.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  part: {
+    type: String,
+    default: "full",
+  },
+});
+</script>
+
+<template>
+  <nuxt-img :src="`/images/logos/efiriyad/${part}.svg`" />
+</template>
