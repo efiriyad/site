7d8c8af4f7e463663fa655e61f73b7b997c66a64
rmenai <rami.menai@outlook.com>
2022-08-21 22:57:54 +0300

    Arrange terms in account store

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -5,16 +5,25 @@ import { defineStore } from "pinia";
 export const useAccountStore = defineStore("account", {
   state: () => {
     return {
-      lvb: "",
-      lvc: "",
-      specialities: ["math", "physics", "svt"],
+      languages: { lva: "english_lva", lvb: "arabic_lvb" },
+      options: { op1: "", op2: "" },
+      specialties: { sp1: "math", sp2: "physics", sp3: "svt" },
     };
   },
 
   actions: {
-    updateLanguages(lvb, lvc) {
-      this.lvb = lvb;
-      this.lvc = lvc;
+    updateLanguages(languages) {
+      this.languages.lva = languages.lva;
+      this.languages.lvb = languages.lvb;
+    },
+    updateOptions(options) {
+      this.options.op1 = options.op1;
+      this.options.op2 = options.op2;
+    },
+    updateSpecialties(specialties) {
+      this.specialties.sp1 = specialties.sp1;
+      this.specialties.sp2 = specialties.sp2;
+      this.specialties.sp3 = specialties.sp3;
     },
   },
 
