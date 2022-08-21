0f5a1d3cf9ffd013e9b18b5a86f63362211c200f
rmenai <rami.menai@outlook.com>
2022-08-21 22:59:23 +0300

    Finalize student choices list

--- src/composables/constants.ts
+++ src/composables/constants.ts
@@ -1,18 +1,33 @@
 export const themeColors = { light: "#f7f7f8", dark: "#121212" };
 
 // Settings.
-export const speChoices = {
-  lvb: [
-    { label: "lessons.none", value: "" },
-    { label: "lessons.arabic", value: "arabic" },
-    { label: "lessons.german", value: "german" },
-    { label: "lessons.spanish", value: "spanish" },
-  ],
-  lvc: [
-    { label: "lessons.none", value: "" },
-    { label: "lessons.sport_op", value: "sport_op" },
-    { label: "lessons.art", value: "art" },
-  ],
+export const studentChoices = {
+  languages: {
+    lva: [
+      { label: "lessons.english", value: "english_lva" },
+      { label: "lessons.arabic", value: "arabic_lva" },
+    ],
+    lvb: [
+      { label: "lessons.english", value: "english_lvb" },
+      { label: "lessons.arabic", value: "arabic_lvb" },
+      { label: "lessons.german", value: "german_lvb" },
+      { label: "lessons.spanish", value: "spanish_lvb" },
+    ],
+  },
+  options: {
+    op1: [
+      { label: "lessons.none", value: "" },
+      { label: "lessons.sport_op", value: "sport_op" },
+      { label: "lessons.art", value: "art" },
+      { label: "lessons.arabic", value: "arabic_lvc" },
+      { label: "lessons.spanish", value: "spanish_lvc" },
+      { label: "lessons.german", value: "german_lvc" },
+    ],
+    op2: [
+      { label: "lessons.none", value: "" },
+      { label: "lessons.math", value: "math_op" },
+    ],
+  },
   specialties: [
     { label: "lessons.math", value: "math" },
     { label: "lessons.physics", value: "physics" },
