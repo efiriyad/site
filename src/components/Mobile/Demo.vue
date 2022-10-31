d531378c941b95ffb3051d33919696c6b51b4355
rmenai <rami.menai@outlook.com>
2022-10-31 17:13:03 +0300

    Add more content to MobileDemo

--- src/components/Mobile/Demo.vue
+++ src/components/Mobile/Demo.vue
@@ -11,9 +11,13 @@ export default {
 </script>
 
 <template>
-  <k-app theme="ios" :dark="false">
+  <k-app class="min-h-max" theme="ios" :dark="false" style="min-height: 0">
     <k-page>
-      <k-navbar title="Efiriyad PWA" />
+      <div class="flex h-full flex-col items-center justify-center">
+        <k-navbar title="Efiriyad" class="h-18 first:pt-7" />
+        <div class="grow"></div>
+        <MobileTabBar :href="false" notification="1" class="bottom-0 mb-1 max-w-md pt-1" />
+      </div>
     </k-page>
   </k-app>
 </template>
