e18e0b8023c4c55d229d79e7c696a9b46bf6fc80
rmenai <rami.menai@outlook.com>
2022-10-31 18:06:47 +0300

    Add stores for locale and theme

--- src/stores/client/locale.ts
+++ src/stores/client/locale.ts
@@ -2,18 +2,36 @@
 // Using a localeMixin, i18n updates instantly in every page.
 import { defineStore } from "pinia";
 
-export const useLocaleStore = defineStore("locale", {
-  state: () => {
-    return {
-      lang: "en",
-    };
-  },
-
-  actions: {
-    updateLang(lang) {
-      this.lang = lang;
+// Use a wrapper in order to first load the localStorage.
+export const useLocaleStore = () => {
+  const localStorageStore = defineStore("locale", {
+    state: () => {
+      return {
+        lang: "",
+      };
     },
-  },
 
-  persist: true,
-});
+    actions: {
+      updateLang(lang) {
+        if (process.client) {
+          localStorage.setItem("locale", lang);
+        }
+
+        this.lang = lang;
+      },
+    },
+
+    persist: false,
+  });
+
+  const store = localStorageStore();
+
+  // Load values from localStorage.
+  if (process.client) {
+    if (!store.lang) {
+      store.lang = localStorage.getItem("locale") || "en";
+    }
+  }
+
+  return store;
+};
---
+++ src/stores/client/theme.ts
@@ -0,0 +1,1 @@
+// The color theme is automatically stored by `colorMode`.
